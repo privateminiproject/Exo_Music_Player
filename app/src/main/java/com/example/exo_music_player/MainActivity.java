b2d23fb503a6268064b8b3d6da1b6c1bfd08b9ee
rupeshjadhav <jadhavrupesh22@gmail.com>
2019-09-07 18:29:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exo_music_player/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.exo_music_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayerView playerView;
+    private SimpleExoPlayer player;
+    private Object dataSourceFactory;
+    private long playbackPosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.video_view);
+        String uri="https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
+        initializePlayer(uri);
+
+    }
+
+
+    private void initializePlayer(String AudioUri) {
+        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(this),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+
+        Uri uri = Uri.parse(AudioUri);
+        MediaSource mediaSource = new ExtractorMediaSource(uri,
+                new DefaultDataSourceFactory(this, "Music"),
+                new DefaultExtractorsFactory(), null, null);
+        player.prepare(mediaSource);
+        playerView.setPlayer(player);
+        player.setPlayWhenReady(true);
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+
+            player.release();
+            player = null;
+        }
+    }
+
+
+}
